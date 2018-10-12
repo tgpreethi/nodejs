5822b946af216d898d2dbee9e7ba2684a1bea52e
Preethi Thimma Govarthanarajan <38863409+tgpreethi@users.noreply.github.com>
2018-10-11 18:50:50 -0700

    Update app.js

--- app.js
+++ app.js
@@ -54,11 +54,11 @@ var db_pwd  = "cmpe281" ;
 var db_name = "test" ;
 */
 
-var db_host = (process.env.mongodb_host || "localhost" ) ;
-var db_port = (process.env.mongodb_port || "27017" ) ;
+var db_host = (process.env.mongodb_host || "ds129823.mlab.com" ) ;
+var db_port = (process.env.mongodb_port || "29823" ) ;
 var db_user = (process.env.mongodb_user || "cmpe281" ) ;
 var db_pwd  = (process.env.mongodb_pwd  || "cmpe281" ) ;
-var db_name = (process.env.mongodb_name || "test" ) ;
+var db_name = (process.env.mongodb_name || "heroku_w0sdjv5q" ) ;
    
 
 var db = new DB(db_name,
